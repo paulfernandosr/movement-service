62b6319d9f0c4d72b15d7f20c1a41b3a3ebe7771
Paul Fernando Sacaca Ramirez <pfsr98@gmail.com>
2022-07-19 18:58:14 -0500

    update for project 3

--- src/test/java/com/nttdata/movementservice/MovementServiceApplicationTests.java
+++ src/test/java/com/nttdata/movementservice/MovementServiceApplicationTests.java
@@ -1,12 +1,10 @@
 package com.nttdata.movementservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MovementServiceApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
