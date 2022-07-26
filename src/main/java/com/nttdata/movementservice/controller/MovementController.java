373e8563449e1abd68be85a8bf12f532d16fa891
Paul Fernando Sacaca Ramirez <pfsr98@gmail.com>
2022-07-25 19:16:11 -0500

    update for project 4

--- src/main/java/com/nttdata/movementservice/controller/MovementController.java
+++ src/main/java/com/nttdata/movementservice/controller/MovementController.java
@@ -4,7 +4,6 @@ import com.nttdata.movementservice.dto.MovementDto;
 import com.nttdata.movementservice.dto.request.*;
 import com.nttdata.movementservice.service.IMovementService;
 import com.nttdata.movementservice.util.Constants;
-import com.nttdata.movementservice.util.RequestValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
@@ -22,7 +22,6 @@ import java.net.URI;
 public class MovementController {
 
     private final IMovementService service;
-    private final RequestValidator validator;
 
     @GetMapping(Constants.GET_ALL_METHOD)
     public Mono<ResponseEntity<Flux<MovementDto>>> getAll() {
@@ -53,100 +52,90 @@ public class MovementController {
     }
 
     @GetMapping(Constants.GET_BANK_ACCOUNT_FEES_METHOD)
-    public Mono<ResponseEntity<Flux<MovementDto>>> getBankAccountFees(@RequestBody FeesBankAccountDto feesBankAccount) {
+    public Mono<ResponseEntity<Flux<MovementDto>>> getBankAccountFees(@Valid @RequestBody FeesBankAccountDto feesBankAccount) {
         return Mono.just(ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(service.getBankAccountFees(feesBankAccount)));
     }
 
     @PostMapping(Constants.REGISTER_DEPOSIT_METHOD)
-    public Mono<ResponseEntity<MovementDto>> registerDeposit(@RequestBody TransactionBankAccountDto movement, final ServerHttpRequest request) {
-        return validator.validate(movement)
-                .flatMap(validatedMovement -> service.registerDeposit(validatedMovement)
-                        .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(registeredMovement)));
+    public Mono<ResponseEntity<MovementDto>> registerDeposit(@Valid @RequestBody TransactionBankAccountDto movement, final ServerHttpRequest request) {
+        return service.registerDeposit(movement)
+                .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(registeredMovement));
     }
 
     @PostMapping(Constants.REGISTER_WITHDRAWAL_METHOD)
-    public Mono<ResponseEntity<MovementDto>> registerWithdrawal(@RequestBody TransactionBankAccountDto movement, final ServerHttpRequest request) {
-        return validator.validate(movement)
-                .flatMap(validatedMovement -> service.registerWithdrawal(validatedMovement)
-                        .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(registeredMovement)));
+    public Mono<ResponseEntity<MovementDto>> registerWithdrawal(@Valid @RequestBody TransactionBankAccountDto movement, final ServerHttpRequest request) {
+        return service.registerWithdrawal(movement)
+                .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(registeredMovement));
     }
 
     @PostMapping(Constants.REGISTER_DEBIT_CARD_PAYMENT_METHOD)
-    public Mono<ResponseEntity<MovementDto>> registerDebitCardPayment(@RequestBody MovementDebitCardDto movement, final ServerHttpRequest request) {
-        return validator.validate(movement)
-                .flatMap(validatedMovement -> service.registerDebitCardPayment(validatedMovement)
-                        .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(registeredMovement)));
+    public Mono<ResponseEntity<MovementDto>> registerDebitCardPayment(@Valid @RequestBody MovementDebitCardDto movement, final ServerHttpRequest request) {
+        return service.registerDebitCardPayment(movement)
+                .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(registeredMovement));
     }
 
     @PostMapping(Constants.REGISTER_DEBIT_CARD_WITHDRAWAL_METHOD)
-    public Mono<ResponseEntity<MovementDto>> registerDebitCardWithdrawal(@RequestBody MovementDebitCardDto movement, final ServerHttpRequest request) {
-        return validator.validate(movement)
-                .flatMap(validatedMovement -> service.registerDebitCardWithdrawal(validatedMovement)
-                        .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(registeredMovement)));
+    public Mono<ResponseEntity<MovementDto>> registerDebitCardWithdrawal(@Valid @RequestBody MovementDebitCardDto movement, final ServerHttpRequest request) {
+        return service.registerDebitCardWithdrawal(movement)
+                .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(registeredMovement));
     }
 
     @PostMapping(Constants.REGISTER_TRANSFER_TO_OWN_BANK_ACCOUNT_METHOD)
-    public Mono<ResponseEntity<MovementDto>> registerTransferToOwnBankAccount(@RequestBody TransferBankAccountDto movement, final ServerHttpRequest request) {
-        return validator.validate(movement)
-                .flatMap(validatedMovement -> service.registerTransferToOwnBankAccount(validatedMovement)
-                        .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(registeredMovement)));
+    public Mono<ResponseEntity<MovementDto>> registerTransferToOwnBankAccount(@Valid @RequestBody TransferBankAccountDto movement, final ServerHttpRequest request) {
+        return service.registerTransferToOwnBankAccount(movement)
+                .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(registeredMovement));
     }
 
     @PostMapping(Constants.REGISTER_TRANSFER_TO_THIRD_PARTIES_BANK_ACCOUNT_METHOD)
-    public Mono<ResponseEntity<MovementDto>> registerTransferToThirdPartiesBankAccount(@RequestBody TransferBankAccountDto movement, final ServerHttpRequest request) {
-        return validator.validate(movement)
-                .flatMap(validatedMovement -> service.registerTransferToThirdPartiesBankAccount(validatedMovement)
-                        .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(registeredMovement)));
+    public Mono<ResponseEntity<MovementDto>> registerTransferToThirdPartiesBankAccount(@Valid @RequestBody TransferBankAccountDto movement, final ServerHttpRequest request) {
+        return service.registerTransferToThirdPartiesBankAccount(movement)
+                .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(registeredMovement));
     }
 
     @PostMapping(Constants.REGISTER_CREDIT_PAYMENT)
-    public Mono<ResponseEntity<MovementDto>> registerCreditPayment(@RequestBody MovementCreditDto movement, final ServerHttpRequest request) {
-        return validator.validate(movement)
-                .flatMap(validatedMovement -> service.registerCreditPayment(validatedMovement)
-                        .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(registeredMovement)));
+    public Mono<ResponseEntity<MovementDto>> registerCreditPayment(@Valid @RequestBody MovementCreditDto movement, final ServerHttpRequest request) {
+        return service.registerCreditPayment(movement)
+                .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(registeredMovement));
     }
 
     @PostMapping(Constants.REGISTER_CREDIT_CARD_PAYMENT)
-    public Mono<ResponseEntity<MovementDto>> registerCreditCardPayment(@RequestBody MovementCreditDto movement, final ServerHttpRequest request) {
-        return validator.validate(movement)
-                .flatMap(validatedMovement -> service.registerCreditCardPayment(validatedMovement)
-                        .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(registeredMovement)));
+    public Mono<ResponseEntity<MovementDto>> registerCreditCardPayment(@Valid @RequestBody MovementCreditDto movement, final ServerHttpRequest request) {
+        return service.registerCreditCardPayment(movement)
+                .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(registeredMovement));
     }
 
     @PostMapping(Constants.REGISTER_CREDIT_CARD_CONSUMPTION)
-    public Mono<ResponseEntity<MovementDto>> registerCreditCardConsumption(@RequestBody MovementCreditDto movement, final ServerHttpRequest request) {
-        return validator.validate(movement)
-                .flatMap(validatedMovement -> service.registerCreditCardConsumption(validatedMovement)
-                        .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(registeredMovement)));
+    public Mono<ResponseEntity<MovementDto>> registerCreditCardConsumption(@Valid @RequestBody MovementCreditDto movement, final ServerHttpRequest request) {
+        return service.registerCreditCardConsumption(movement)
+                .map(registeredMovement -> ResponseEntity.created(URI.create(request.getURI() + Constants.SLASH + registeredMovement.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(registeredMovement));
     }
 
     @PutMapping(Constants.UPDATE_BY_ID_METHOD)
-    public Mono<ResponseEntity<MovementDto>> updateById(@PathVariable(Constants.ID) String id, @RequestBody MovementDto movement) {
-        return validator.validate(movement)
-                .flatMap(validatedMovement -> service.updateById(id, validatedMovement)
-                        .map(updatedMovement -> ResponseEntity.ok()
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(updatedMovement)));
+    public Mono<ResponseEntity<MovementDto>> updateById(@PathVariable(Constants.ID) String id, @Valid @RequestBody MovementDto movement) {
+        return service.updateById(id, movement)
+                .map(updatedMovement -> ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(updatedMovement));
     }
 
     @DeleteMapping(Constants.DELETE_BY_ID_METHOD)
--- src/main/java/com/nttdata/movementservice/exception/GlobalErrorAttributes.java
+++ src/main/java/com/nttdata/movementservice/exception/GlobalErrorAttributes.java
@@ -8,11 +8,14 @@ import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.reactive.function.server.ServerRequest;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -24,29 +27,31 @@ public class GlobalErrorAttributes extends DefaultErrorAttributes {
         Map<String, Object> errorAttributes = super.getErrorAttributes(request, options);
         errorAttributes.remove(Constants.REQUEST_ID);
         errorAttributes.replace(Constants.TIMESTAMP, LocalDateTime.now());
-
         if (error instanceof DomainException) {
             DomainException domainException = (DomainException) error;
             HttpStatus status = domainException.getStatus();
             errorAttributes.replace(Constants.STATUS, status.value());
             errorAttributes.replace(Constants.ERROR, status.getReasonPhrase());
-            errorAttributes.put(Constants.MESSAGE, domainException.getMessage());
-
-            if (domainException instanceof BadRequestException) {
-                BadRequestException badRequestException = (BadRequestException) domainException;
-                List<ValidationDto> validations = badRequestException.getValidations();
-                String validationsString = JacksonUtil.objectToString(validations);
-                errorAttributes.put(Constants.VALIDATIONS, validations);
-                log.error(Constants.FIVE_CURLY_BRACKETS, Constants.VIOLATED_CONSTRAINTS, Constants.IN,
-                        badRequestException.getRequestClass(), Constants.COLON, validationsString);
-            } else {
-                log.error(error.getMessage());
-            }
+            errorAttributes.replace(Constants.MESSAGE, domainException.getMessage());
+        } else if (error instanceof WebExchangeBindException) {
+            WebExchangeBindException webExchangeBindException = (WebExchangeBindException) error;
+            List<ValidationDto> validations = webExchangeBindException.getFieldErrors().stream().map(this::buildValidationDto).collect(Collectors.toList());
+            String validationsString = JacksonUtil.objectToString(validations);
+            errorAttributes.replace(Constants.MESSAGE, Constants.VIOLATED_CONSTRAINTS);
+            errorAttributes.put(Constants.VALIDATIONS, validations);
+            log.error(Constants.FIVE_CURLY_BRACKETS, Constants.VIOLATED_CONSTRAINTS, Constants.IN,
+                    webExchangeBindException.getTarget(), Constants.COLON, validationsString);
         } else {
             log.error(error.getMessage());
         }
-
         return errorAttributes;
+    }
+
+    private ValidationDto buildValidationDto(FieldError fieldError) {
+        return ValidationDto.builder()
+                .field(fieldError.getField())
+                .message(fieldError.getDefaultMessage())
+                .build();
     }
 
 }
